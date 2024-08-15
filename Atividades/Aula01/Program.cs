3ce0c74d8e8eb828a22fd67e0b986406a1329ac0
Kalil Massignani da Rosa <406157@unoesc.lan>
2024-08-14 22:07:12 -0300

    Primeiro projeto .net

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Comentário de uma linha
+/*
+    Comentário de múltiplas linhas
+*/
+
+//Declarando Variável
+int number;
+//Atribuindo Valor
+number = 10;
+
+//Declarando Constante
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo string concatenada
+//Métodos tem nomes de verbos
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias");
