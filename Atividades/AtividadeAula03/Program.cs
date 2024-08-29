a3f114487533270e06608d5fef9fa118651a182e
Kalil Massignani da Rosa <406157@unoesc.lan>
2024-08-28 21:59:23 -0300

    Atividade aula 03

---
+++ Atividades/AtividadeAula03/Program.cs
@@ -0,0 +1,25 @@
+﻿int evenCounter;
+int oddCounter;
+
+int[] even = new int[50];
+int[] odd = new int[50];
+
+for(int i = 0; i <= 100; i++){
+    if(i%2==0){
+        even[i] = i ;
+        evenCounter=+1;
+    }else{
+        odd[i] = i;
+        oddCounter=+ 1;
+    }
+}
+
+Console.WriteLine("Even numbers: ");
+for(int i = 0; i <= even.Length; i++){
+    Console.WriteLine(even[i]);
+}
+
+Console.WriteLine("Odd numbers: ");
+for(int i = 0; i <= odd.Length; i++){
+    Console.WriteLine(odd[i]);
+}
