fc0c20fb107b269ad7624026687637878de4758b
Kalil Massignani da Rosa <406157@unoesc.lan>
2024-09-11 21:32:33 -0300

    Listas Genéricas

---
+++ Atividades/AtividadeLivro/Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeLivro
+{
+    public class Book
+    {
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public int NumberPage { get; set; }
+    }
+}
