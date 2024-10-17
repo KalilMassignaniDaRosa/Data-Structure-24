7f13fdc97f1d3b72571359ab1f73a8df54cf1a13
Kalil Massignani da Rosa <406157@unoesc.lan>
2024-10-16 21:16:57 -0300

    Quick Sort

---
+++ Atividades/AlgoritmosOrdenacao/QuickSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using QuickSort;
+
+int[] values = [0, 12, 7, 1, 2, 8];
+
+Console.WriteLine("Array: ");
+QuickSort.QuickSort.Print<int>(values);
+
+QuickSort.QuickSort.Sort<int>(values, 0, values.Length-1);
+
+Console.WriteLine("Sorted with Quick: ");
+QuickSort.QuickSort.Print(values);
